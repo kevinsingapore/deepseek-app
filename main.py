8309d3b5f0990d1f2e7e5c2974a3e76590d60017
付强 <fuqiang_coding@coding.net>
2025-04-15 11:54:15 +0800

    mysql function updated

--- main.py
+++ main.py
@@ -15,8 +15,29 @@ print(logPath)
 
 
 # 初始化连接 MySQL数据库
-init.connect_to_mysql(config)
+conn = init.connect_to_mysql(config)
+cursor = conn.cursor()
 
 
 # monitor.get_log(logPath)
+# 获取日志，读取日志，增量读取日志 
+def get_log(log_path,conn):
+    if os.path.exists(log_path):
+        with open(log_path, 'r') as file:
+            lines = file.readlines()
+            # return log
+            for line in lines: 
+                # 如果line内容中包含warning或error，则推送到mysql与rabbtimq中               
+                if "warning" in line or "error" in line:
+                    # 推送到mysql
+                    sqlStr = "INSERT INTO messages (content) VALUES (%s)"
+                    cursor.execute(sqlStr, (line.strip()))
+                    conn.commit()
+                    
+                    # 推送到rabbitmq中
+                    # print("推送到rabbitmq中")
+    else:
+        print("日志文件不存在")
+        
+get_log(logPath,conn)
 
